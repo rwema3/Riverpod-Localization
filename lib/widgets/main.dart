6210dd510e3c924d2b27d33b52c2cf76104151b9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 21:11:27 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -61,7 +61,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     final DateFormat dateFormat = DateFormat.yMd(ref.read(localeProvider).toString()).add_jms();
 
     String _supportedLocales = ref.read(supportedLocalesProvider).toString();
-    String _platformLocale = ref.read(platformLocaleProvi
+    String _platformLocale = ref.read(platformLocaleProvid
       
 
      
