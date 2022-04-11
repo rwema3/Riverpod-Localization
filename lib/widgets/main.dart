30e9445c2aa2afc8ea7075c1f7e011ad60c57966
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 18:12:57 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -70,7 +70,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Riverpod Localization')
+        title: const Text('Riverpod Localization'
       
       
          
