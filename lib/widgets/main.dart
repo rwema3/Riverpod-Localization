3f42901628206cdf263ccaef796083588a841e6c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 21:57:59 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -79,7 +79,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           children: [
             Expanded(
               child: ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth:
+                constraints: const BoxConstraints(maxWidth: 
                  
                    
                   
