a26ece7c7d92b21df74e182cf44e139513aff60b
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 21:23:59 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -66,7 +66,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
     print("Supported locales: " + _supportedLocales);
     print("Platform Locale: " + _platformLocale);
-    print("Current Locale: " + _
+    print("Current Locale: " + _c
 
      
        
