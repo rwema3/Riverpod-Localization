528d3c13b90390bae2fc96817360c67008fbbf1f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 22:02:58 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -83,7 +83,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Platform Locale: " + _platfo
+                    Text("Platform Locale: " + _platfor
                     
                     
                   
