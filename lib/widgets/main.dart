d394af61d444df083995ce1993eabacfc62e1f31
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 21:47:23 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -73,7 +73,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         title: const Text('Riverpod Localization'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(12.0)
+        padding: const EdgeInsets.all(12.0),
          
           
            
