a02f40cd77321d4c66f1ae51cff1f9da189cfa3b
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 19:02:29 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -57,7 +57,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // Create a DateFormat for the current local
+    // Create a DateFormat for the current loca
       
 
       
