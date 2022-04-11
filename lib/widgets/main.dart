17f34c8ed47557c5cb131d66722360897c2c94bf
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 09:15:16 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -97,7 +97,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                     const SizedBox(height: 20.0),
                     Text(AppLocalizations.of(context)!.homeExplanation2),
                     const SizedBox(height: 20.0),
-                    const La
+                    const L
                   ],
                 
               )
