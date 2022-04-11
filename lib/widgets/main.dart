d0e105c6a4c1d8fc98c1de65eb49c5e5cb1ea49a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 09:21:38 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -94,7 +94,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                     Text(dateFormat.format(DateTime.now())),
                     const SizedBox(height: 20.0),
                     Text(AppLocalizations.of(context)!.homeExplanation),
-                    const Siz
+                    const Si
                     
                     
                     
