e540a7c9c81835a4728d39c9e7f278b59172388d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-11 22:10:19 +0200

    Update main.dart

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -88,8 +88,8 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                     Text("Locale via Localizations: " + Localizations.localeOf(context).toString()),
                     const SizedBox(height: 5),
                     Text("Locale via Riverpod State: " + _currentLocale),
-                    const SizedBox(height: 20.0)
-                    
+                    const SizedBox(height: 20.0),
+                    Text(
                     
                     
                     
