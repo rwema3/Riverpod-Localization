8069bccfdf01b5cc40e74b2fbf99364d4f0d949b
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-18 14:48:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,8 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   void initState() {
     super.initState();
     var widgetsBinding = WidgetsBinding.instance;
+
+    String _supportedLocales = ref.read(supportedLocalesProvider).toString();
     String _platformLocale = ref.read(platformLocaleProvider).toString();
     String _currentLocale = ref.watch(localeProvider).toString();
 
