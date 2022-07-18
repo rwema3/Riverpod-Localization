94cd839b1c36da0675eabf6ee3d9a0e065989925
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-18 14:48:25 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   void initState() {
     super.initState();
     var widgetsBinding = WidgetsBinding.instance;
+    print("Current Locale: " + _currentLocale);
 
     return Scaffold(
       appBar: AppBar(
