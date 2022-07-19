d4818b632feb66cf4ecf81472ecabfde0d5e7518
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-19 20:58:30 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Get supported locales
  
 
 }
