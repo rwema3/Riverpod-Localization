5182b838fa70109cc5a1b0306a8c3a79171c8b65
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-19 20:58:08 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ class MyApp extends ConsumerWidget {
     List<Locale> _supportedLocales = ref.read(supportedLocalesProvider);
 
     // Watch the current locale and rebuild on change
-    Locale _locale = ref.watch(localeProvider);
  
 
 }
