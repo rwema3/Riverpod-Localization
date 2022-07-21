24a2cd77dbee6c9e9aea8610b54c2be4861033b8
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-21 13:45:14 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,9 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         padding: const EdgeInsets.all(12.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-                Text(dateFormat.format(DateTime.now())),
+          children: [
+            Expanded(
+                    Text(dateFormat.format(DateTime.now())),
                     const SizedBox(height: 20.0),
                     Text(AppLocalizations.of(context)!.homeExplanation),
                     const SizedBox(height: 20.0),
