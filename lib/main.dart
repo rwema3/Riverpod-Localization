3a627e8659e25fb3d2e7df068f5e34675eebb796
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-21 13:41:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -66,8 +66,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
     print("Supported locales: " + _supportedLocales);
     print("Platform Locale: " + _platformLocale);
-    print("Current Locale: " + _currentLocale);
-
+    
               const LanguagePicker(),
                   ],
                 ),
