b5a7035e8b455b4638b17437bd06c4910195e837
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-21 13:42:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -56,8 +56,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    
+  
     
               const LanguagePicker(),
                   ],
