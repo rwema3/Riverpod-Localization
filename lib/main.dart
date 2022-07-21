cdbe656957b222b35ebdf47a3ad9ac35d1ff98df
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-21 13:44:48 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Riverpod Localization'),
                 const LanguagePicker(),
                   ],
                 ),
