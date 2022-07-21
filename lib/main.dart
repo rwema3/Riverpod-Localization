5b95bff74e7723548f5130e433cf57ff45651bd2
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-21 13:43:53 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,7 @@ class HomeScreen extends ConsumerStatefulWidget {
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
+  void initState() {
   
     
               const LanguagePicker(),
