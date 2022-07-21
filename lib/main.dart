e51d018b2690c3dc9ebbeefc5792f95180cff93d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-21 13:42:31 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,6 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    var widgetsBinding = WidgetsBinding.instance;
   
     
               const LanguagePicker(),
