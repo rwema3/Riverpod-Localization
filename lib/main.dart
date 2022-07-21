5e781526f7577ba3fffc1a9adaa1f4484c82a343
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-21 13:42:50 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,10 +35,6 @@ class MyApp extends ConsumerWidget {
   }
 }
 
-class HomeScreen extends ConsumerStatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-
     
               const LanguagePicker(),
                   ],
