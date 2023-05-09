b768aaac42802e9fa21765eef883977d9e7f6d42
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-09 23:54:39 +0200

    Future<bool>

--- lib/utils/json_local_sync.dart
+++ lib/utils/json_local_sync.dart
@@ -6,3 +6,4 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// Save and Get a Json [Map<String, dynamic>] from local persistent storage
 class JsonLocalSync {
   /// Save a Json object to local persistent storage
+  static Future<bool> save({required String key, required Map<String, dynamic> value}) async {
