e634fffed5f3c3cfb80442a33ac9fb8211f09853
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-09 23:54:04 +0200

    [Map<String, dynamic>]

--- lib/utils/json_local_sync.dart
+++ lib/utils/json_local_sync.dart
@@ -3,3 +3,4 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// JSON Local Sync
+/// Save and Get a Json [Map<String, dynamic>] from local persistent storage
