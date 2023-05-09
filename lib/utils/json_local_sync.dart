68649a74b012963759cb4ac82264516f29ae407a
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-09 23:52:05 +0200

    Plods

--- lib/utils/json_local_sync.dart
+++ lib/utils/json_local_sync.dart
@@ -6,30 +6,3 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// Save and Get a Json [Map<String, dynamic>] from local persistent storage
 class JsonLocalSync {
   /// Save a Json object to local persistent storage
-  static Future<bool> save({required String key, required Map<String, dynamic> value}) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String jsonString = json.encode(value);
-    return await prefs.setString(key, jsonString);
-  }
-
-  /// Get a Json object from local persistent storage
-  static Future<Map<String, dynamic>?> get({required String key}) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (!prefs.containsKey(key)) {
-      return null;
-    }
-
-    String? jsonString = prefs.getString(key);
-    if (jsonString == null) {
-      print('Unable to getString for key: "$key".');
-      return null;
-    }
-    Map<String, dynamic> value = jsonDecode(jsonString);
-    return value;
-  }
-
-  static Future<bool> delete({required String key}) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return await prefs.remove(key);
-  }
-}
