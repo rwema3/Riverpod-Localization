58d7ae27f4929180d88f1f7a9fa9448de6e5acf6
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-10 23:28:03 +0200

    shared_preferences

--- lib/utils/json_local_sync.dart
+++ lib/utils/json_local_sync.dart
@@ -1,2 +1,13 @@
 import 'dart:convert';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// JSON Local Sync
+/// Save and Get a Json [Map<String, dynamic>] from local persistent storage
+class JsonLocalSync {
+  /// Save a Json object to local persistent storage
+  static Future<bool> save({required String key, required Map<String, dynamic> value}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String jsonString = json.encode(value);
+    return await prefs.setString(key, jsonString);
+  }
